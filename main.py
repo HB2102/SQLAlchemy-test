e6dcb6ed9b2f082ffd38ec9339c09eb6a2ce6721
Hossein <hossienbehzadi1381@gmail.com>
2023-12-20 14:42:14 +0330

    adding all the tables and random data insertion into each table

--- main.py
+++ main.py
@@ -29,16 +29,68 @@ class DB:
         id = Column('id', Integer, primary_key=True, unique=True, autoincrement=True)
 
 
-    class People(SubClass, base):
-        __tablename__ = 'people'
+    class Fields(SubClass, base):
+        __tablename__ = 'fields'
+        name = Column('name', String(100))
+        student = relationship('Students', backref='field')
+        classes1 = relationship('Classes', backref='field2')
+
+    class Teachers(SubClass, base):
+        __tablename__ = 'teachers'
         first_name = Column('first_name', String(50))
         last_name = Column('last_name', String(50))
-        position_id = Column('position_id', Integer, ForeignKey('position.id'))
+        classes2 = relationship('Classes', backref='field3')
 
 
-    class Position(SubClass, base):
-        __tablename__ = 'position'
+    class Students(SubClass, base):
+        __tablename__ = 'students'
+        first_name = Column('first_name', String(50))
+        last_name = Column('last_name', String(50))
+        field_id = Column('field_id', Integer, ForeignKey('fields.id'))
+        classes = relationship('Classes', secondary='student_class', back_populates='students')
+
+    class Classes(SubClass, base):
+        __tablename__ = 'classes'
         name = Column('name', String(50))
-        people = relationship('People', backref='position', back_populates='')
+        teacher_id = Column('teacher_id', Integer, ForeignKey('teachers.id'))
+        field_id = Column('field_id', Integer, ForeignKey('fields.id'))
+        students = relationship('Students', secondary='student_class', back_populates='classes')
 
+
+
+    class StudentClass(SubClass, base):
+        __tablename__ = 'student_class'
+        student_id = Column('student_id', Integer, ForeignKey('students.id'))
+        class_id = Column('class_id', Integer, ForeignKey('classes.id'))
+
+
+if __name__ == '__main__':
+    db = DB()
+    db.create_session()
+    db.create_all_tables()
+    # field = db.Fields(name='Math')
+    # db.session.add(field)
+    # db.session.commit()
+    # student = db.Students(first_name='Ali', last_name='Alizadeh')
+    # db.session.add(student)
+    # db.session.commit()
+    # teacher = db.Teachers(first_name='Ali', last_name= 'Alizadeh')
+    # db.session.add(teacher)
+    # db.session.commit()
+    # db.session.query(DB.Fields).filter(DB.Fields.id == 1).delete()
+    # db.session.commit()
+    #
+    # print(student.field_id)
+
+    # classroom = db.Classes(name='A', field_id= 2, teacher_id=teacher.id)
+    # db.session.add(classroom)
+    # db.session.commit()
+
+    # student = db.session.query(db.Students).one()
+    # classs = db.session.query(db.Classes).one()
+    #
+    # classs.students = [student]
+    # db.session.commit()
+
+    # print(classs.students)
 
